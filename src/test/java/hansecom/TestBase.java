aad55eae33da2e96b279336f743af249aabe4139
veronikapodgol <veronika.podgol@gmail.com>
2022-01-30 20:12:10 +0300

    Initial commit

---
+++ src/test/java/hansecom/TestBase.java
@@ -0,0 +1,37 @@
+package hansecom;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class TestBase {
+    @BeforeAll
+    static void setUp() {
+       //SelenideLogger.addListener("AllureSelenide", new AllureSelenide());  // листенер аллюра
+
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true); //видели что происходит
+        capabilities.setCapability("enableVideo", true); // видео записывает
+        Configuration.browserCapabilities = capabilities;
+        // в блоке DesiredCapabilities конфигурируем удаленный запуск
+    }
+
+   @AfterEach
+   void addAttachments() {
+        //Attach.screenshotAs("Last screenshot");
+        //Attach.pageSource();
+        //Attach.browserConsoleLogs();
+        //Attach.addVideo();
+        closeWebDriver();
+    }
+}
